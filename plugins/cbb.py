aa984dcb3d271f05737c83e47e0944c7ab464644
Arvi Mehta <84314637+mehtaarvi@users.noreply.github.com>
2021-09-22 07:27:02 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -131,7 +131,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor"
+                        "SOURCE CODE", url="https://github.com/mehtaarvi/Image-Editor"
                     )
                 ],
             ]
@@ -148,7 +148,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor"
+                        "SOURCE CODE", url="https://github.com/mehtaarvi/Image-Editor"
                     )
                 ],
             ]
